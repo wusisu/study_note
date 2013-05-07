91d2a84559ac3a8dadfdbd2c5b3d0bd5e5cdc7f0
Wu Sisu <wusisu@qq.com>
2013-05-07 23:56:46 +0800

    Friend table is enough. resource id is simple, which is the localid in chat table. And there are several types of msg, so i have to consider them one by one. Now my majoy problem is that i donot fimilar to html.

--- MMsaver/MMsaver.py
+++ MMsaver/MMsaver.py
@@ -2,16 +2,18 @@
 import os,sys
 import sqlite3
 import hashlib
+import shutil
+import BeautifulSoup
+import time
 
-
-CODING='utf-8'
+CODING='gb2312'
 class MMsaver:
 
 
     def __init__(self):
-        self.find_root_dir()
+        if not self.find_root_dir():print "cannot find the dir"
         self.init_dbworker()
-        self.analyze_db()
+        self.basely_analyze_db()
         self.init_output_dir()
 
     def dothejob(self):
@@ -19,17 +21,66 @@ class MMsaver:
             self.output_chat(ec)
 
 
-    def analyze_db(self):
+    def basely_analyze_db(self):
         self.get_chat_tables()
         self.get_md5s()
 
 
     def output_chat(self,usrmd5):
-        dirname = usrmd5[0:7]+self.md5_dict[usrmd5].encode(CODING,'ignore')
-        usrdir = os.path.join(self.outputddir,dirname)
+        dirname = usrmd5[0:7]+'_'+self.md5_dict[usrmd5]
+        usrdir = os.path.join(self.outputddir,dirname)#.encode(CODING,'ignore'))
         os.mkdir(usrdir)
-        
+        os.mkdir(os.path.join(usrdir,'audio'))
+        os.mkdir(os.path.join(usrdir,'img'))
 
+        t_cs = self.dbworker.cursor()
+        t_cs.execute('select * from Chat_%s'%usrmd5)
+        t_msg_list = t_cs.fetchall()
+
+        u_xhtml_file = open(os.path.join(usrdir,'Message.html'),'w')
+        u_xhtml_file.write("<html><head></head><body>")
+        for _msg in t_msg_list:
+            u_xhtml_file.write('<ul>')
+            u_xhtml_file.write('<li>'+time.localtime(int(_msg[3])).__str__()+'</li>')
+            
+            if _msg[7]==10000:#10000号是腾讯的消息号啊！
+                u_xhtml_file.write('<li>'+_msg[4].encode(CODING,'ignore')+'</li>')
+
+            u_xhtml_file.write('</ul>')
+        u_xhtml_file.write("</body></html>")
+        '''
+        u_aud_count=0
+        u_pic_count=0
+        u_aud_list,u_pic_list = self.get_data_list(usrmd5)
+
+        for _epic in u_pic_list:
+            shutil.copy(os.path.join(self.rootdir,'Img',usrmd5,_epic),os.path.join(usrdir,'img',_epic[:-3]+'jpg'))
+
+        for _eaud in u_aud_list:
+            t_source_file = open(os.path.join(self.rootdir,'Audio',usrmd5,_eaud),'rb')
+            t_raw = t_source_file.read()
+            t_source_file.close()
+            t_target_file = open(os.path.join(usrdir,'audio',_eaud[:-3]+'amr'),'wb')
+            t_target_file.write("#!AMR\n"+t_raw)
+            t_target_file.close()
+        '''
+    def get_data_list(self,usrmd5):
+        res_aud_list = []
+        res_pic_list = []
+        for _ef in os.listdir(os.path.join(self.rootdir,'Audio',usrmd5)):
+            if _ef.endswith('.aud'):
+                res_aud_list.append(_ef)
+        for _ef in os.listdir(os.path.join(self.rootdir,'Img',usrmd5)):
+            if _ef.endswith('.pic'):
+                res_pic_list.append(_ef)
+        
+        res_aud_list.sort(self.filename_compare)
+        res_pic_list.sort(self.filename_compare)
+        result = (res_aud_list,res_pic_list)
+        return result
+
+    def filename_compare(self,fna,fnb):
+        return int(os.path.splitext(fna)[0]) - int(os.path.splitext(fnb)[0])
 
     def get_chat_tables(self):
         cursor = self.dbworker.cursor()
@@ -43,22 +94,22 @@ class MMsaver:
     def get_md5s(self):
         cursor = self.dbworker.cursor()
         result = cursor.execute('select UsrName,NickName from Friend')
-        md5_maker = hashlib.md5()
         self.md5_dict={}
         for _eun in result:
+            md5_maker = hashlib.md5()
             md5_maker.update(_eun[0])
             self.md5_dict[md5_maker.hexdigest()]=_eun[1]
 
     def cleanup_for_exit(self):
-        pass
+        self.dbworker.commit()
+        self.dbworker.close()
 
     def init_output_dir(self):
         self.outputddir = os.path.join(os.path.curdir,'output')
-        if not os.path.exists(self.outputddir):
-            os.mkdir(self.outputddir)
-        elif not os.path.isdir(self.outputddir):
-            os.remove(self.outputddir)
-            os.mkdir(self.outputddir)
+        if os.path.exists(self.outputddir):
+            shutil.rmtree(self.outputddir)
+        os.mkdir(self.outputddir)
+
 
     def init_dbworker(self):
         self.dbworker = sqlite3.connect(os.path.join(self.rootdir,'DB','MM.sqlite'))
@@ -105,6 +156,7 @@ class MMsaver:
 
 
 if __name__=='__main__':
+
     def compare_md5(a,b):
         cp = int(a[0],16) - int(b[0],16)
         if cp>0:
@@ -115,8 +167,6 @@ if __name__=='__main__':
             return -1
 
     a = MMsaver()
-    a.analyze_db()
-    #print a.chat_tables
     f = open('md5.txt','w')
 
     md5l = []
@@ -124,12 +174,16 @@ if __name__=='__main__':
         md5l.append((e,a.md5_dict[e].encode(CODING,'ignore')))
     md5l.sort(compare_md5)
     for e in md5l:
-        f.write(e.__str__())
+        f.write(e[0]+' '+e[1])
         f.write('\n')
     f.close()
+
+    index=0
     f = open('chats.txt','w')
     for e in a.chat_tables:
+        #if e.startswith('f1'):print index
+        index+=1
         f.write(e)
         f.write('\n')
     f.close()
-    a.output_chat(a.chat_tables[0])
+    a.output_chat(a.chat_tables[6])
