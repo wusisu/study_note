5e1c99d2056a802a86adc627904de742138b1d80
Wu Sisu <wusisu@qq.com>
2013-05-08 21:46:51 +0800

    now almost can run

--- MMsaver/MMsaver.py
+++ MMsaver/MMsaver.py
@@ -6,7 +6,7 @@ import shutil
 import BeautifulSoup
 import time
 
-CODING='utf-8'
+CODING='gb2312'
 class MMsaver:
 
 
@@ -27,23 +27,28 @@ class MMsaver:
 
 
     def output_chat(self,usrmd5):
-        dirname = usrmd5[0:7]+'_'+self.md5_dict[usrmd5]
+        dirname = usrmd5[0:7]+'_'+self.md5_dict[usrmd5][0]
         usrdir = os.path.join(self.outputddir,dirname)#.encode(CODING,'ignore'))
         os.mkdir(usrdir)
         os.mkdir(os.path.join(usrdir,'audio'))
         os.mkdir(os.path.join(usrdir,'img'))
+
+        self.copy_all_files(usrmd5,usrdir)
 
         t_cs = self.dbworker.cursor()
         t_cs.execute('select * from Chat_%s'%usrmd5)
         t_msg_list = t_cs.fetchall()
         u_xhtml_file = open(os.path.join(usrdir,'Message.html'),'w')
         u_xhtml_file.write("<html><head></head><body>")
+        yourname = ''
+        if not self.md5_dict[usrmd5][1]:
+            yourname = self.md5_dict[usrmd5][0]
         for _msg in t_msg_list:
-            item_string = self.get_item_string(_msg)
+            item_string = self.get_item_string(_msg,yourname)
             u_xhtml_file.write(item_string)
         u_xhtml_file.write("</body></html>")
     
-    def copy_all_files(self,usrmd5):
+    def copy_all_files(self,usrmd5,usrdir):
         u_aud_list,u_pic_list = self.get_data_list(usrmd5)
         for _epic in u_pic_list:
             shutil.copy(os.path.join(self.rootdir,'Img',usrmd5,_epic),os.path.join(usrdir,'img',_epic[:-3]+'jpg'))
@@ -55,16 +60,40 @@ class MMsaver:
             t_target_file.write("#!AMR\n"+t_raw)
             t_target_file.close()
 
-    def get_item_string(self,msg):
+    def get_item_string(self,msg,your_name=''):
             result = ''
             result += '<ul class="item" id=%d>'%msg[1]
             t_time = time.localtime(int(msg[3]))
             result += '<li class="time">%s</li>'%time.asctime(t_time)
             if msg[7]==10000:#10000号是腾讯的消息号啊！
-                result += '<li class="system_msg">%s</li>'%msg[4].encode(CODING,'ignore')
+                result += '<li class="body" id="system_msg">%s</li>'%msg[4].encode(CODING,'ignore')
             else:
-                pass
+                msg_body=msg[4].encode(CODING,'ignore')
+                if msg[8]==0:#自己发的
+                    result += '<li class="who" id="self">wo:</li>'
+                else:
+                    #写名字
+                    if your_name=='':#群里
+                        t_name_index = msg_body.find(':\n')
+                        result += '<li class="who" id="others">%s:</li>'%msg_body[:t_name_index]
+                        msg_body = msg_body[t_name_index:]
+                    else:
+                        result += '<li class="who" id="you">%s:</li>'%your_name
+
+                if msg[7]==49:#49是app
+                    result += '<li class="body" id="app">%s</li>'%"app"
+                elif msg[7]==47:#47是表情。emoji的那个。
+                    result += '<li class="body" id="emoji">%s</li>'%"emoji"
+                elif msg[7]==34:#voice
+                    result += '<li class="body" id="voice">%d.amr</li>'%msg[1]
+                elif msg[7]==3:
+                    result += '<li class="body" id="pic"><img src="img\\%d.jpg"/></li>'%msg[1]
+                elif msg[7]==1:
+                    result += '<li class="body" id="text">%s</li>'%msg_body
+                else:
+                    result += '<li class="error">%d can not parse</li>'%msg[7]
             result += '</ul>'
+            return result
 
     def get_data_list(self,usrmd5):
         res_aud_list = []
@@ -100,7 +129,7 @@ class MMsaver:
         for _eun in result:
             md5_maker = hashlib.md5()
             md5_maker.update(_eun[0])
-            self.md5_dict[md5_maker.hexdigest()]=_eun[1]
+            self.md5_dict[md5_maker.hexdigest()]=(_eun[1],_eun[0].endswith('@chatroom'))
 
     def cleanup_for_exit(self):
         self.dbworker.commit()
@@ -173,7 +202,7 @@ if __name__=='__main__':
 
     md5l = []
     for e in a.md5_dict:
-        md5l.append((e,a.md5_dict[e].encode(CODING,'ignore')))
+        md5l.append((e,a.md5_dict[e][0].encode(CODING,'ignore')))
     md5l.sort(compare_md5)
     for e in md5l:
         f.write(e[0]+' '+e[1])
--- MMsaver/test.py
+++ MMsaver/test.py
@@ -1,3 +1,6 @@
-if True or   \
-	False:
-	print 'a'
+from BeautifulSoup import BeautifulSoup as B
+from BeautifulSoup import BeautifulStoneSoup as S
+soup = S(open("temp.xml",'r').read())
+f = open("new.xml",'w')
+f.write(soup.prettify())
+f.close()
